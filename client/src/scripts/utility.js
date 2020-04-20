86990fec456ebcb2cb190370cd6f558bd90ab8c2
leodinh <leo.atdinh@gmail.com>
2020-04-20 01:46:50 -0400

    added format for input phone,currency

--- client/src/scripts/utility.js
+++ client/src/scripts/utility.js
@@ -1,6 +1,92 @@
+import web3 from "web3";
+import Decimal from "decimal.js";
+import React from "react";
+import NumberFormat from "react-number-format";
 export const updateObject = (oldObject, updatedValues) => {
   return {
     ...oldObject,
     ...updatedValues
   };
 };
+
+export const convertWeiToEther = amount => {
+  return amount === 0 ? 0 : web3.utils.fromWei(amount, "ether");
+};
+
+export const convertWeiToUSD = (amount, rate) => {
+  const ether = convertWeiToEther(amount);
+  console.log(ether);
+  console.log(rate);
+  const usdAmount = new Decimal(ether).mul(new Decimal(rate));
+  return usdAmount.toString();
+};
+
+export const convertEtherToWei = amount => {
+  console.log(amount);
+  return !amount ? 0 : web3.utils.toWei(amount, "ether");
+};
+
+export function USDFormatCustom(props) {
+  const { inputRef, onChange, ...other } = props;
+
+  return (
+    <NumberFormat
+      {...other}
+      getInputRef={inputRef}
+      onValueChange={values => {
+        onChange({
+          target: {
+            name: props.name,
+            value: values.value
+          }
+        });
+      }}
+      thousandSeparator
+      isNumericString
+      prefix="$"
+    />
+  );
+}
+export function ETHFormatCustom(props) {
+  const { inputRef, onChange, ...other } = props;
+
+  return (
+    <NumberFormat
+      {...other}
+      getInputRef={inputRef}
+      onValueChange={values => {
+        onChange({
+          target: {
+            name: props.name,
+            value: values.value
+          }
+        });
+      }}
+      thousandSeparator
+      isNumericString
+    />
+  );
+}
+
+export function PhoneFormatCustom(props) {
+  const { inputRef, onChange, ...other } = props;
+
+  return (
+    <NumberFormat
+      {...other}
+      getInputRef={inputRef}
+      onValueChange={values => {
+        onChange({
+          target: {
+            name: props.name,
+            value: values.value
+          }
+        });
+      }}
+      thousandSeparator
+      isNumericString
+      format="+1 (###) ###-####"
+      mask="_"
+    />
+  );
+}
