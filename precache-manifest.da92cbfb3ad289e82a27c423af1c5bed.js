66cf23cb06a07be774be1eaea2a1e7e39ff085c5
leodinh <leo.atdinh@gmail.com>
2020-04-21 02:05:57 -0400

    Updates

---
+++ precache-manifest.da92cbfb3ad289e82a27c423af1c5bed.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "989d42de6c276810289ed414eeb9420d",
+    "url": "/microfinance-dapp/index.html"
+  },
+  {
+    "revision": "3c31cd9ddb9018fa2c10",
+    "url": "/microfinance-dapp/static/css/10.23467497.chunk.css"
+  },
+  {
+    "revision": "d388f326cc68dfb81035",
+    "url": "/microfinance-dapp/static/css/11.8e17a3d7.chunk.css"
+  },
+  {
+    "revision": "00a5f306d689e9739b88",
+    "url": "/microfinance-dapp/static/css/12.2fc08492.chunk.css"
+  },
+  {
+    "revision": "3f44f3dc5f78e02b42bf",
+    "url": "/microfinance-dapp/static/css/13.a112ed89.chunk.css"
+  },
+  {
+    "revision": "7b68d1795e3f78109523",
+    "url": "/microfinance-dapp/static/css/7.9b647238.chunk.css"
+  },
+  {
+    "revision": "25539ec8964a97167b9f",
+    "url": "/microfinance-dapp/static/css/9.c8031c79.chunk.css"
+  },
+  {
+    "revision": "e2e31bb1287bd952b77b",
+    "url": "/microfinance-dapp/static/css/main.927fdcde.chunk.css"
+  },
+  {
+    "revision": "c8717640c9bfb27ae542",
+    "url": "/microfinance-dapp/static/js/0.b6152806.chunk.js"
+  },
+  {
+    "revision": "ca58877648a03b6235ec",
+    "url": "/microfinance-dapp/static/js/1.0103d6ef.chunk.js"
+  },
+  {
+    "revision": "3c31cd9ddb9018fa2c10",
+    "url": "/microfinance-dapp/static/js/10.ba082646.chunk.js"
+  },
+  {
+    "revision": "d388f326cc68dfb81035",
+    "url": "/microfinance-dapp/static/js/11.91d1eb09.chunk.js"
+  },
+  {
+    "revision": "00a5f306d689e9739b88",
+    "url": "/microfinance-dapp/static/js/12.fa92da8b.chunk.js"
+  },
+  {
+    "revision": "3f44f3dc5f78e02b42bf",
+    "url": "/microfinance-dapp/static/js/13.798e5834.chunk.js"
+  },
+  {
+    "revision": "476585961414a5defbab",
+    "url": "/microfinance-dapp/static/js/14.5715f491.chunk.js"
+  },
+  {
+    "revision": "fe8426110ef168b811a8",
+    "url": "/microfinance-dapp/static/js/2.b1c2dd36.chunk.js"
+  },
+  {
+    "revision": "14f95fcace1b3eb2cca8",
+    "url": "/microfinance-dapp/static/js/3.b50ce75f.chunk.js"
+  },
+  {
+    "revision": "54d709726e337abd44f0",
+    "url": "/microfinance-dapp/static/js/6.4da46c38.chunk.js"
+  },
+  {
+    "revision": "7b68d1795e3f78109523",
+    "url": "/microfinance-dapp/static/js/7.41d66070.chunk.js"
+  },
+  {
+    "revision": "54d72204073b76967138",
+    "url": "/microfinance-dapp/static/js/8.205cd005.chunk.js"
+  },
+  {
+    "revision": "25539ec8964a97167b9f",
+    "url": "/microfinance-dapp/static/js/9.36c0c92d.chunk.js"
+  },
+  {
+    "revision": "e2e31bb1287bd952b77b",
+    "url": "/microfinance-dapp/static/js/main.208f6850.chunk.js"
+  },
+  {
+    "revision": "af735b92e591d03d14c6",
+    "url": "/microfinance-dapp/static/js/runtime-main.03384037.js"
+  },
+  {
+    "revision": "2da281ad2b985f250d6cf205ef2c56ba",
+    "url": "/microfinance-dapp/static/media/address.2da281ad.svg"
+  },
+  {
+    "revision": "4e84d0d98a2539dcc186da371873b93b",
+    "url": "/microfinance-dapp/static/media/coupon_cover.4e84d0d9.jpg"
+  },
+  {
+    "revision": "7070b7c5e151d21926e225576f0ea13c",
+    "url": "/microfinance-dapp/static/media/cover.7070b7c5.jpg"
+  },
+  {
+    "revision": "4a9ac9291a300ad18fc263ecbb64cf72",
+    "url": "/microfinance-dapp/static/media/expand.4a9ac929.svg"
+  },
+  {
+    "revision": "8e54df70940ebca7d1d9575e90f507a6",
+    "url": "/microfinance-dapp/static/media/heart-icon.8e54df70.gif"
+  },
+  {
+    "revision": "a3eec9e2ded3d14299aac73588754bfc",
+    "url": "/microfinance-dapp/static/media/icon.a3eec9e2.png"
+  },
+  {
+    "revision": "40ab1b0a47f24ca2c99fe1722ee1d395",
+    "url": "/microfinance-dapp/static/media/loading-heart.40ab1b0a.svg"
+  },
+  {
+    "revision": "e4c0aeaee68768173cba12cdcea1f0b4",
+    "url": "/microfinance-dapp/static/media/metamask.e4c0aeae.png"
+  },
+  {
+    "revision": "18b20c04b2b89d25e38a9620e7cf84d6",
+    "url": "/microfinance-dapp/static/media/person.18b20c04.svg"
+  },
+  {
+    "revision": "62c39e7f78e7760cf0cc6db9f1d38ed7",
+    "url": "/microfinance-dapp/static/media/phone.62c39e7f.svg"
+  }
+]);
